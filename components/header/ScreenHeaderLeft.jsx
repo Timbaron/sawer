6cd2c8b687145b2982c8f5aa3bf955c78bf95d95
timbaron <timothy.akiode@student.aul.edu.ng>
2023-05-02 15:02:13 +0100

    design app logo

--- components/header/ScreenHeaderLeft.jsx
+++ components/header/ScreenHeaderLeft.jsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { COLORS, FONT, SIZES } from '../../constants'
 
 const ScreenHeaderLeft = () => {
   return (
     <View>
-      <Text>ScreenHeaderLeft</Text>
+      <Text style={styles.containerText}>SAWER</Text>
     </View>
   )
 }
 
 export default ScreenHeaderLeft
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  containerText: {
+    fontSize: SIZES.large,
+    fontFamily: FONT.bold,
+    letterSpacing: SIZES.small / 5,
+    color: COLORS.primary
+  }
+})
